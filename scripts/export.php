f19b64bb84014b7a05c2eb6b5a6f064ed9bb55d8
Daniel Romano <daniel.romano@034-web.library.mcgill.ca>
2015-07-22 14:10:57 -0400

    Added scripts/export.php to assist with exporting a survey.

---
+++ scripts/export.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once(__DIR__ . '/inc.php');
+
+// Check for survey id.
+if (count($argv) == 1){
+	echo "Usage: php export.php $survey_id\n\n";
+	die();	
+}
+
+survey_export($argv[1]);
+
