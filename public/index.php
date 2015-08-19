5baabf0318e478865d002ecf5cf59092bb967335
Daniel Romano <daniel.romano@034-web.library.mcgill.ca>
2015-08-19 15:40:19 -0400

    Added support for forcing the survey to be presented, despite the threshold and session count.

--- public/index.php
+++ public/index.php
@@ -10,6 +10,13 @@ $conf = config();
 $survey_id = @$_GET['id'] ? $_GET['id'] : $conf['default-survey-id'];
 $url = @$_GET['url'] ? $_GET['url'] : redirect_and_die($conf['default-redirect-url']);
 
+// If URL begins with uppercase HTTP, this will force the survey to be displayed.
+// We are relying on this obscure hack in order to leave the request URL undisturbed.
+$force_survey = strpos($url, 'HTTP') === 0 ? true : false;
+if ($force_survey){
+	redirect_and_die("survey.php?id=$survey_id&url=$url");
+}
+
 // Verify if application is enabled or within the configured date-range.
 if (!$conf['enabled'] || !today_is_between($conf['begin_date'], $conf['end_date'])) redirect_and_die($url);
 
