69518164b70f673749e07268c4a5c642c26755bb
Daniel Romano <daniel.romano@034-qa-web.library.mcgill.ca>
2015-09-01 09:28:45 -0400

    The thanks page now makes use of the app's template.

--- public/thanks.php
+++ public/thanks.php
@@ -16,13 +16,25 @@ if (!$survey) redirect_and_die($_REQUEST['url']);
 	'css' => survey_config($survey->id, 'css'),
 	'head' => '<meta http-equiv="refresh" content="' . $survey->thanks_redirect_after_seconds . '; url=' . $_REQUEST['url'] . '"/>',
 )); ?>
-<div class="thank-you">
-	<div class="thank-you-title">
-		Thank you!
+<div class="survey">
+	<div class="survey-logos">
+		<div class="survey-logo"><img src="static/logo.png"/></div>
+		<div class="survey-mines-logo"><img src="static/mines-logo.png"/></div>
+		<div class="clear"></div>
 	</div>
-	<div class="thank-you-message">
-		You will automatically be redirected to the resource you requested in <?php eko($survey->thanks_redirect_after_seconds); ?> seconds.<br/>
-		Or you can click here: <a href="<?php eko($_REQUEST['url']); ?>"><?php eko($_REQUEST['url']); ?></a>
+	<div class="survey-name">
+		<?php echo $survey->name; ?>
+	</div>
+	<div class="survey-notes">
+		<div class="thank-you">
+			<div class="thank-you-title">
+				Thank you!
+			</div>
+			<div class="thank-you-message">
+				You will automatically be redirected to the resource you requested in <?php eko($survey->thanks_redirect_after_seconds); ?> seconds.<br/>
+				Or you can click here: <a href="<?php eko($_REQUEST['url']); ?>"><?php eko($_REQUEST['url']); ?></a>
+			</div>
+		</div>
 	</div>
 </div>
 <?php echo template_close(); ?>
