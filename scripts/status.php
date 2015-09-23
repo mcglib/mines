8515108bd40b237e571394d6b11391a30cf13ecb
Daniel Romano <daniel.romano@034-web.library.mcgill.ca>
2015-09-23 10:43:18 -0400

    Improved scripts/status.php report.

--- scripts/status.php
+++ scripts/status.php
@@ -17,11 +17,14 @@ $survey = survey($survey_id);
 
 // Get submission count.
 $submission_count = count(survey_submissions($survey_id));
+$sessions_per_submissions = round($survey->session_count / $submission_count, 2);
 
 // Report
-echo "Date range: {$survey->begin_date} to {$survey->end_date}\n";
+echo "Survey: {$survey->id}\n";
+echo "Active: {$survey->begin_date} to {$survey->end_date}\n";
 echo "Sessions: {$survey->session_count}\n";
 echo "Threshold: {$survey->threshold}\n";
 echo "Submissions: $submission_count\n";
+echo "Sessions / Submissions: $sessions_per_submissions\n";
 echo "\n";
 
