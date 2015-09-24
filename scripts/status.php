23409a18a913914c3477a2a27d5a7c80144df4a1
Daniel Romano <daniel.romano@034-qa-web.library.mcgill.ca>
2015-09-24 09:22:00 -0400

    Status reporting now inlcudes the last status as well.

--- scripts/status.php
+++ scripts/status.php
@@ -17,14 +17,41 @@ $survey = survey($survey_id);
 
 // Get submission count.
 $submission_count = count(survey_submissions($survey_id));
+
+// Current date
+$date = date("Y-m-d H:m:s");
+
+// Determine sessions divided by submissions
 $sessions_per_submissions = round($survey->session_count / $submission_count, 2);
 
+// Determine last-status file path
+$LAST_REPORT_FILE_PATH = __DIR__ . '/../tmp/last-status.txt';
+
 // Report
-echo "Survey: {$survey->id}\n";
-echo "Active: {$survey->begin_date} to {$survey->end_date}\n";
-echo "Sessions: {$survey->session_count}\n";
-echo "Threshold: {$survey->threshold}\n";
-echo "Submissions: $submission_count\n";
-echo "Sessions / Submissions: $sessions_per_submissions\n";
-echo "\n";
+$current_report = <<<EOS
+Status Date and Time: 	{$date}
+Survey: 		{$survey->id}
+Active: 		{$survey->begin_date} to {$survey->end_date}
+Sessions: 		{$survey->session_count}
+Threshold: 		{$survey->threshold}
+Submissions: 		{$submission_count}
+Sessions / Submissions: {$sessions_per_submissions}
+EOS;
+
+$last_report = '';
+if (file_exists($LAST_REPORT_FILE_PATH)){
+	$last_report = file_get_contents($LAST_REPORT_FILE_PATH);
+}
+
+// Write the current report to $LAST_STATUS_FILE_PATH.
+file_put_contents($LAST_REPORT_FILE_PATH, $current_report);
+
+echo <<<EOS
+*CURRENT REPORT*
+{$current_report}
+
+*LAST REPORT*
+{$last_report}
+
+EOS;
 
